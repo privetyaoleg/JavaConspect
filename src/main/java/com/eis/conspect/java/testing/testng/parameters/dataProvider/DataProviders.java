44f3db8a22b7bfd029d78fdf4a13855bf344f228
arutski <mydak13@bk.ru>
2018-08-05 21:07:36 +0300

    TestNG

--- src/main/java/com/eis/conspect/java/testing/testng/parameters/dataProvider/DataProviders.java
+++ src/main/java/com/eis/conspect/java/testing/testng/parameters/dataProvider/DataProviders.java
@@ -1,8 +1,11 @@
 package com.eis.conspect.java.testing.testng.parameters.dataProvider;
 
+import com.eis.conspect.java.testing.junit.rule.examples.second.annotations.SingleParameterDataSource;
 import org.testng.annotations.DataProvider;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,7 +21,35 @@ public class DataProviders {
     /**
      * Data provider must return Object[][]/Object[] or Iterator<Object[]>/Iterator<Object>
      */
+
     @DataProvider
+    private Object[][] arrayDataProvider(Method method) throws NoSuchFieldException, IllegalAccessException {
+        if (method.isAnnotationPresent(SingleParameterDataSource.class)) {
+            if (method.getParameterTypes().length != 1) {
+                throw new RuntimeException("Method should have a single parameter: " + method);
+            }
+
+            SingleParameterDataSource dataSource = method.getAnnotation(SingleParameterDataSource.class);
+            Class clazz = dataSource.clazz();
+            if (clazz.equals(void.class)) {
+                clazz = method.getDeclaringClass();
+            }
+
+            Field field = clazz.getField(dataSource.value());
+            Object[] data = (Object[]) field.get(null);
+
+            Object[][] result = new Object[data.length][];
+            for (int i = 0; i < data.length; i++) {
+                result[i] = new Object[]{data[i]};
+            }
+            return result;
+        } else {
+            throw new RuntimeException("There is no @SingleParameterDataSource annotation on method: " + method);
+        }
+    }
+
+    @DataProvider
+
     private Iterator<Object[]> usersFromFile() throws IOException {
         List<Object[]> data = new ArrayList<>();
         readProperties(DATA_PROVIDER_PATH).forEach((key, value) -> data.add(new Object[]{key, value}));
