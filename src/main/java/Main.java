dc87d22d2fd6a08a28a32da099125ab4f0997163
arutski <mydak13@bk.ru>
2018-08-27 22:47:00 +0300

    Creational patterns

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,27 @@ public class Main {
     public static void main(String[] args) {
 
 
+        isNum(40);
 
 
+    }
+
+    public static boolean isNum(int num) {
+
+        int x = num;
+
+        for (int i = 0; i < 32; i++) {
+
+            System.out.println(Integer.toBinaryString(num));
+
+            if (num == 3) {
+                return false;
+            } else if (num == 1) {
+                return true;
+            } else {
+                num = num >>> 1;
+            }
+        }
+        return false;
     }
 }
