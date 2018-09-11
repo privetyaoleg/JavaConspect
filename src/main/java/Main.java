137a2d83d2c2b355708608bd6e96c8049d337372
arutski <mydak13@bk.ru>
2018-09-11 21:59:16 +0300

    Patterns update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int a = 5;
+        int b = 18;
 
-        isNum(40);
-
+        List<Integer> oddNumbers = getOddNumbers(a, b);
+        for (int i = 0; i < oddNumbers.size(); i++) {
+            System.out.println(oddNumbers.get(i));
+        }
 
     }
 
-    public static boolean isNum(int num) {
-
-        int x = num;
-
-        for (int i = 0; i < 32; i++) {
-
-            System.out.println(Integer.toBinaryString(num));
-
-            if (num == 3) {
-                return false;
-            } else if (num == 1) {
-                return true;
-            } else {
-                num = num >>> 1;
+    private static void printOddNumbers(int a, int b) {
+        while (a < b) {
+            if (a++ % 2 == 1) {
+                System.out.println(a);
             }
         }
-        return false;
     }
+
+    private static List<Integer> getOddNumbers(int a, int b) {
+        List<Integer> oddNumbers = new ArrayList<>();
+        while (a < b) {
+            if (++a % 2 == 1) {
+                oddNumbers.add(a);
+            }
+        }
+        return oddNumbers;
+    }
+
+    private static int getSumNumbers(int a, int b) {
+        int result = 0;
+        while (++a < b) {
+            result += a;
+        }
+        return result;
+    }
+
 }
